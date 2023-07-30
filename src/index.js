06919b6136eae359c1264fed7fd6ea61b936ee25
Ricurrency <lucaslohhangyih@gmail.com>
2023-07-30 19:29:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import {
   getAuth,
-  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
-import {collection, addDoc, setDoc, doc, getFirestore} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js"
+// import {collection, addDoc, setDoc, doc, getFirestore} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js"
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyC_fj2Rek6QIX3hF6U_L7boZwXXOrLOPr4",
   authDomain: "team-codeon.firebaseapp.com",
@@ -16,15 +17,16 @@ const firebaseApp = initializeApp({
 });
 const auth = getAuth(firebaseApp);
 
-const db = getFirestore(firebaseApp)
-onAuthStateChanged(auth, (user) => {
-  if (user != null) {
-    console.log("Logged in!");
-  } else {
-    console.log("No user");
-  }
-});
 
-await setDoc(doc(db, "userinfo", "userpwd"), {
-  username: "12345",
-});
+createUserWithEmailAndPassword(auth, "codeon-site@gmail.com", "code.beyond.limits")
+
+signInWithEmailAndPassword(auth, "codeon-site@gmail.com", "code.beyond.limits")
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("signed in")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
