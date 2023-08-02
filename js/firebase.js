8c51fce8716d5e7bff7fe41de4831942bdfed875
Ricurrency <lucaslohhangyih@gmail.com>
2023-08-02 22:12:57 +0800

    a

--- js/firebase.js
+++ js/firebase.js
@@ -1,32 +1,48 @@
-import { initializeApp } from "https://www.gstatic.com/firebase/9.0.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebase/9.0.0/firebase-app.js';
-import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 
+// import {collection, addDoc, setDoc, doc, getFirestore} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js"
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyC_fj2Rek6QIX3hF6U_L7boZwXXOrLOPr4",
-    authDomain: "team-codeon.firebaseapp.com",
-    projectId: "team-codeon",
-    storageBucket: "team-codeon.appspot.com",
-    messagingSenderId: "791878813030",
-    appId: "1:791878813030:web:600ca7058ef4503b42b4a7",
-    measurementId: "G-CNGJSS771N"
+  apiKey: "AIzaSyC_fj2Rek6QIX3hF6U_L7boZwXXOrLOPr4",
+  authDomain: "team-codeon.firebaseapp.com",
+  projectId: "team-codeon",
+  storageBucket: "team-codeon.appspot.com",
+  messagingSenderId: "791878813030",
+  appId: "1:791878813030:web:600ca7058ef4503b42b4a7",
+  measurementId: "G-CNGJSS771N",
 });
 const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
-db.collection('todos').getDocs();
-const todosCol = collection(db, 'todos')
-const snapshot = await getDocs(todosCol)
 
-// Detect auth state
-auth.onAuthStateChanged(user => {
 
-});
-onAuthStateChanged(auth, user => {
-    if (user != null) {
-        console.log('logged in!')
-    }
+// createUserWithEmailAndPassword(auth, "codeon-site@gmail.com", "code.beyond.limits")
 
-    else {
-        console.log('No user')
-    }
-})
+/*
+signInWithEmailAndPassword(auth, "codeon-site@gmail.com", "code.beyond.limits")
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+*/
+
+function login() {
+  signInWithEmailAndPassword(auth, document.getElementById("username-input-field").value, document.getElementById("password-input-field").value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    const UserUID = user.uid
+    localStorage.setItem("firb-log-usr",user.uid)
+  })
+  .catch(() => {
+    document.getElementById("incorrect-details-error").style.visibility = "visible";
+  });
+}
+window.login = login;
